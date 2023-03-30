ea3539c4660b4024b99a97847606bf60e2195c87
UD-sht <udshrestha48@gmail.com>
2023-03-30 08:18:52 +0545

    commit

--- NCC/lab7/Program.cs
+++ NCC/lab7/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace lab5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          
+        }
+    }
+    
+}
