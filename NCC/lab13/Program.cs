633fdf21c4a5e0b0bdd730f9c48d3df4d18813cf
UD-sht <udshrestha48@gmail.com>
2023-03-31 07:46:24 +0545

    change name

--- NCC/lab13/Program.cs
+++ NCC/lab13/Program.cs
@@ -15,7 +15,7 @@ using System.Threading.Tasks;
         {
 List<Dog> dogs = new List<Dog>() 
             {
-             new Dog { Name = "Rex", Age = 4 },
+             new Dog { Name = "Ren", Age = 4 },
              new Dog { Name = "Sean", Age = 0 },
              new Dog { Name = "Stacy", Age = 3 }
             };            var names = dogs.Select(x => x.Name);
